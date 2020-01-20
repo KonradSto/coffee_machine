c1d7354f93445bd813a4b78e32892b3d9ea473c9
KonradSto <stolarczyk.konrad@outlook.com>
2020-01-20 21:12:54 +0100

    adds private method validating sufficient ingredients

--- src/main/java/service/BeverageMakerImp.java
+++ src/main/java/service/BeverageMakerImp.java
@@ -18,7 +18,7 @@ public class BeverageMakerImp implements BeverageMaker {
     public void makeCoffee(String beverage, Status currentStatus) {
         List<String> recipe = fileReaderImp.readRecipeLinesToList("src\\main\\resources\\" + beverage.replaceAll("\\s","")+ ".txt");
         List<String> step;
-        if (recipe != null) {
+        if (recipe != null && checkSufficientIngredients(recipe, currentStatus)) {
             for (String line : recipe) {
                 step = recipeParser.parseRecipeLine(line);
                 drawIngredient(step.get(1), step.get(2), Integer.parseInt(step.get(3)), currentStatus);
@@ -49,5 +49,44 @@ public class BeverageMakerImp implements BeverageMaker {
                     status.setCocoaAmount(status.getCocoaAmount() - quantity);
             }
         }
+    }
+
+    private boolean checkSufficientIngredients(List<String> recipe, Status currentStatus) {
+        List<String> step;
+        for (String line : recipe) {
+            step = recipeParser.parseRecipeLine(line);
+            if (step.get(1).equals("GRINDING") || step.get(1).equals("HEATING")){
+                switch (step.get(2)) {
+                    case "COFFEE":
+                        if (currentStatus.getCoffeeAmount() - Integer.parseInt(step.get(3)) < 0){
+                            printMissingIngredientMessage(step.get(2));
+                            return false;
+                        }
+                        break;
+                    case "MILK":
+                        if (currentStatus.getMilkAmount() - Integer.parseInt(step.get(3)) < 0){
+                            printMissingIngredientMessage(step.get(2));
+                            return false;
+                        }
+                        break;
+                    case "WATER":
+                        if (currentStatus.getWaterAmount() - Integer.parseInt(step.get(3)) < 0){
+                            printMissingIngredientMessage(step.get(2));
+                            return false;
+                        }
+                        break;
+                    case "COCOA":
+                        if (currentStatus.getCocoaAmount() - Integer.parseInt(step.get(3)) < 0){
+                            printMissingIngredientMessage(step.get(2));
+                            return false;
+                        }
+                }
+            }
+        }
+        return true;
+    }
+
+    private void printMissingIngredientMessage(String ingredient){
+        System.out.println("NOT ENOUGH " + ingredient + "! PLEASE REFILL...");
     }
 }
